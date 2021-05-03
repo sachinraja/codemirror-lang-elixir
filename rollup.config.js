d1435f95be68fc75b49a098d3d11e9aba4c625ce
Sachin Raja <sachinraja2349@gmail.com>
2021-05-03 11:02:33 -0700

    feat: elixir language mode

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+const { join } = require("path")
+
+const modeFile = 'index.js'
+
+module.exports = {
+  input: modeFile,
+  output: {
+    file: modeFile.replace(/\.js$/, ".cjs"),
+    format: "cjs"
+  },
+  external: id => !/^(\.?\/|\w:)/.test(id)
+}
